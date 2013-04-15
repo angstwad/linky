475fe2af9c01ed39fd3d591aadce764f69db57b2
Paul Durivage <pauldurivage@gmail.com>
2013-04-15 17:03:40 -0500

    Huge functionality changes, func/class/obj renames

--- linky/db.py
+++ linky/db.py
@@ -1,67 +1,62 @@
 import sqlalchemy.exc
 import sqlalchemy.orm.exc
-
 import exc
-
-from datetime import datetime
-from collections import namedtuple
-
+import gen_key
 from config import MAX_EMAILS_PER_DAY
-from db_def import Session, user_register, user_verified, emails_sent
-from gen_hash import gen_sha_hash
+from db_def import Session, UserRegister, UserVerified, EmailsSent
 from . import logger
 
 
-class register(object):
+class Register(object):
     def __init__(self, email):
         self.session = Session()
         self.email = email
 
-    def __insert_reg_db(self, userhash):
+    def do_register(self):
+        logger.info('Beginning register of email: %s' % self.email)
+        if not self.__in_verified_db():
+            key = gen_key.generate(self.email)
+            return self.__insert_reg_db(key)
+
+    def __in_verified_db(self):
+        logger.info('Checking verified DB for email %s.' % self.email)
+        if self.session.query(UserVerified) \
+            .filter_by(email=self.email) \
+            .count() > 0:
+            logger.info('Registration fail, found in verified DB: %s'
+                        % self.email)
+            self.error = "Email has already been verified."
+            return True
+
+    def __insert_reg_db(self, key):
         logger.info('Beginning insert of %s into user_register DB.'
                     % self.email)
-        new_user = user_register(email=self.email, send_key=userhash,
-                                 create_time=datetime.utcnow(),
-                                 last_login=datetime.utcnow())
+        new_user = UserRegister(email=self.email, send_key=key)
         self.session.add(new_user)
 
         try:
             self.session.commit()
-        except sqlalchemy.exc.IntegrityError as e:
-            logger.warning('Duplicate email found in '
-                           'user_registered table: %s' % self.email)
-            logger.exception(e.message)
+        except sqlalchemy.exc.IntegrityError:
+            self.error = "Email has already been registered."
+            logger.info('Duplicate email found in '
+                        'user_registered table: %s' % self.email)
+            return False
         except Exception as e:
             logger.exception(e.message)
         else:
             logger.info('Email registered, written to DB: %s' % self.email)
-            return {'email': self.email,
-                    'hash': userhash}
-
-    def __in_verified_db(self):
-        logger.info('Checking verified DB for email %s.' % self.email)
-        if self.session.query(user_verified)\
-                       .filter_by(email=self.email)\
-                       .count() > 0:
-            pass
-        else:
+            self.key = key
             return True
 
-    def do_register(self):
-        logger.info('Attempt to register email: %s' % self.email)
-        if not self.__in_verified_db():
-            userhash = gen_sha_hash(self.email)
-            return self.__insert_reg_db(userhash)
-
     def recover(self):
-        q_ver = self.session.query(user_verified)\
+        q_ver = self.session.query(UserVerified)\
                     .filter_by(email=self.email)\
                     .first()
         if q_ver:
             return {'email': q_ver.email,
                     'key': q_ver.email_key}
         else:
-            q_reg = self.session.query(user_register)\
+            q_reg = self.session.query(UserRegister)\
                                 .filter_by(email=self.email)\
                                 .first()
             if q_reg:
@@ -69,7 +64,7 @@ class register(object):
                         'key': q_reg.send_key}
 
 
-class verification(object):
+class Verification(object):
     def __init__(self, key):
         self.key = key
         self.session = Session()
@@ -77,7 +72,7 @@ class verification(object):
     def check_verification(self):
         try:
             logger.info('Check if verified key %s' % self.key)
-            q = self.session.query(user_verified)\
+            q = self.session.query(UserVerified)\
                     .filter_by(email_key=self.key)\
                     .one()
         except (sqlalchemy.orm.exc.MultipleResultsFound,
@@ -91,47 +86,41 @@ class verification(object):
     def run_verification(self):
         try:
             logger.info('Verify key %s' % self.key)
-            q = self.session.query(user_register)\
+            q = self.session.query(UserRegister)\
                     .filter_by(send_key=self.key)\
                     .one()
         except (sqlalchemy.orm.exc.NoResultFound,
                 sqlalchemy.orm.exc.MultipleResultsFound) as e:
             logger.exception(e.message)
         else:
-            if self._reg_db_to_ver_db(q):
+            if self.__move_to_verified(q):
                 return True
 
-    def _reg_db_to_ver_db(self, q):
-        new_record = user_verified(email=q.email, email_key=q.send_key)
+    def __move_to_verified(self, q):
+        new_record = UserVerified(email=q.email, email_key=q.send_key)
+        logger.info('Attempting to add '
+                    'record to verify DB: %s, %s' % (q.email, q.send_key))
+        self.session.add(new_record)
         try:
-            logger.info('Attempting to add '
-                        'record to verify DB: %s, %s' % (q.email, q.send_key))
-            self.session.add(new_record)
-        except BaseException as e:
-            logger.exception(e.message)
+            self.session.commit()
+        except sqlalchemy.exc.IntegrityError:
+            logger.info('Abort commit, key has already been verified: '
+                        '%s' % self.key)
+            self.error = 'Key has already been verified.'
+            return False
         else:
             logger.info('Record added '
                         'to verify DB: %s, %s' % (q.email, q.send_key))
-            try:
-                logger.info('Attempting to delete '
-                            'record from register DB: %s' % q.email)
-                self.session.delete(q)
-            except Exception as e:
-                logger.exception(e.message)
-            else:
-                logger.info('Record deleted, purging from '
-                            'DB with session.commit(): %s' % q.email)
-                self.session.commit()
-                return True
+            return True
 
 
-class mail(object):
+class Mail(object):
     def __init__(self, key):
         self.key = key
         self.session = Session()
 
         try:
-            q = self.session.query(user_verified)\
+            q = self.session.query(UserVerified)\
                     .filter_by(email_key=key)\
                     .one()
         except sqlalchemy.orm.exc.NoResultFound:
@@ -147,7 +136,7 @@ class mail(object):
         def requery():
             try:
                 logger.info('Requery for key: %s', self.key)
-                self.q = self.session.query(emails_sent)\
+                self.q = self.session.query(EmailsSent)\
                              .filter_by(email_key=self.key)\
                              .one()
             except (sqlalchemy.orm.exc.NoResultFound,
@@ -157,13 +146,13 @@ class mail(object):
         try:
             logger.info('Looking for user in emails_sent DB: %s'
                         % self.email_addr)
-            self.q = self.session.query(emails_sent)\
+            self.q = self.session.query(EmailsSent)\
                          .filter_by(email_key=self.key)\
                          .one()
         except sqlalchemy.orm.exc.NoResultFound:
             logger.info('Email has no entry in '
                         'emails_sent DB: %s' % self.email_addr)
-            self.session.add(emails_sent(email=self.email_addr,
+            self.session.add(EmailsSent(email=self.email_addr,
                                          email_key=self.__sendkey,
                                          num_sent=0))
             requery()
