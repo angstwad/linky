b9234ed4cddf956939f6b6dc13352e83929fa3ad
Paul Durivage <pauldurivage@gmail.com>
2013-04-15 16:58:51 -0500

    Move function up.

--- linky/mailgun.py
+++ linky/mailgun.py
@@ -3,7 +3,7 @@ from requests.auth import HTTPBasicAuth
 from . import app, logger
 
 
-class send_email(object):
+class SendEmail(object):
     def __init__(self, email, subject, content):
         self.__apikey = app.config['MAILGUN_API_KEY']
         self.__mailgunURL = app.config['MAILGUN_API_URL']
@@ -17,13 +17,13 @@ class send_email(object):
                                   'subject':  self.__subject,
                                   'text': self.__content}
 
+    def mail_link(self):
+        logger.info('Sending email to %s, Subject: %s' % (self.email,
+                                                          self.__subject))
+        self.__send()
+
     def __send(self):
         r = requests.post(self.__mailgunURL,
                           auth=HTTPBasicAuth('api', self.__apikey),
                           data=self.mailgun_send_dict)
         self.mail_send_status = r.status_code
-
-    def mail_link(self):
-        logger.info('Sending email to %s, Subject: %s' % (self.email,
-                                                          self.__subject))
-        self.__send()
