1561c6517f387ef5e7403c85dfa713ccb8d4d889
Paul Durivage <pauldurivage@gmail.com>
2013-04-17 12:42:13 -0500

    Rename instance vars to meet pep8

--- linky/mailgun.py
+++ linky/mailgun.py
@@ -5,25 +5,25 @@ from . import app, logger
 
 class SendEmail(object):
     def __init__(self, email, subject, content):
-        self.__apikey = app.config['MAILGUN_API_KEY']
-        self.__mailgunURL = app.config['MAILGUN_API_URL']
+        self._apikey = app.config['MAILGUN_API_KEY']
+        self._mailgunURL = app.config['MAILGUN_API_URL']
 
         self.email = email
-        self.__subject = subject
-        self.__content = content
+        self._subject = subject
+        self._content = content
 
         self.mailgun_send_dict = {'from': 'LinkyMail <linkymail@linkyto.me>',
                                   'to': self.email,
-                                  'subject':  self.__subject,
-                                  'text': self.__content}
+                                  'subject':  self._subject,
+                                  'text': self._content}
 
-    def __send(self):
-        r = requests.post(self.__mailgunURL,
-                          auth=HTTPBasicAuth('api', self.__apikey),
+    def _send(self):
+        r = requests.post(self._mailgunURL,
+                          auth=HTTPBasicAuth('api', self._apikey),
                           data=self.mailgun_send_dict)
         self.mail_status_code = r.status_code
         logger.info('Email sent - <%s> - Mailgun Status Code: %s'
                     % (self.email, self.mail_status_code))
 
     def mail_link(self):
-        self.__send()
+        self._send()
