ad1be27588d3f960afb47a0ae6e783533ec04401
Paul Durivage <pauldurivage@gmail.com>
2013-04-17 13:06:04 -0500

    Changes for appropriate recovery -- elimination of redirect from recover to user home page

--- linky/routes.py
+++ linky/routes.py
@@ -1,8 +1,8 @@
+from flask import render_template, request, abort, jsonify, redirect, flash
 import exc
 import db
 import send_link
-from flask import render_template, request, abort, jsonify, redirect, flash
-from registration import RegistrationForm, send_registration_email
+import registration
 import recover
 from . import app
 from . import logger
@@ -14,31 +14,33 @@ def index():
 
 
 @app.route('/register', methods=['GET', 'POST'])
-def register_page():
-    form = RegistrationForm(request.form)
+def registry():
+    form = registration.RegistrationForm(request.form)
     if form.validate_on_submit() and request.method == 'POST':
         reg = db.Register(form.email.data)
         is_success = reg.do_register()
         if is_success:
-            send_registration_email(reg.email, reg.key)
+            registration.send_registration_email(reg.email, reg.key)
             return render_template('register.html', title='Registered!',
-                                   form=form, reg_results=is_success)
+                                   register_active='active', form=form,
+                                   reg_results=is_success)
         elif not is_success:
             return render_template('register.html', title='Oops!',
-                                   form=form, reg_results=is_success,
-                                   error=reg.error)
+                                   register_active='active', form=form,
+                                   reg_results=is_success, error=reg.error)
     else:
         return render_template('register.html', title='Register',
-                               form=form, show_form=True)
+                               register_active='active', form=form,
+                               show_form=True)
 
 
-@app.route('/verify/<veri_code>')
+@app.route('/verify/<veri_code>/')
 def verify(veri_code):
-    verify = db.Verification(veri_code)
-    good_result = verify.run_verification()
-    if not good_result:
+    v = db.Verification(veri_code)
+    v_result = v.run_verification()
+    if not v_result:
         return render_template('verify.html', title="Verify",
-                               code=veri_code, result=verify.error)
+                               code=veri_code, result=v.error)
     else:
         return redirect('/user/%s' % veri_code)
 
@@ -48,33 +50,32 @@ def recovery():
     form = recover.RecoverForm(request.form)
     if form.validate_on_submit() and request.method == 'POST':
         response = recover.do_recover(form.email.data)
-        if isinstance(response['response'], str):
+        if response:
             flash(response['response'])
-            return render_template('recover.html', title="Recover",
-                                   form=form)
         else:
-            redirect('/user/%s' % response['response']['key'])
+            flash('This email does not have an account.', 'error')
+        return render_template('recover.html', title="Recover",
+                               recover_active='active', form=form)
     else:
-            return render_template('recover.html', title="Recover",
-                                   form=form, show_form=True)
+        return render_template('recover.html', title="Recover",
+                               recover_active='active', form=form,
+                               show_form=True)
 
 
 @app.route('/user/<userid>')
 def userpage(userid):
     return render_template('userpage.html', title='User Page',
                            userid=userid)
+
 # TODO: Implement user's home page to get their bookmark
 
 @app.route('/user/<userid>/send', methods=['POST'])
 def send(userid):
     if not isinstance(userid, unicode):
-        print "isinstance"
         abort(401)
     elif not userid.isalnum():
-        print "isalnum"
         abort(401)
     elif not len(userid) == 40:
-        print "userid.len"
         abort(400)
     elif not request.json:
         abort(400)
@@ -89,6 +90,3 @@ def send(userid):
             return jsonify({'status': {'error': e.message}})
         else:
             return jsonify(status='OK')
-
-
-
